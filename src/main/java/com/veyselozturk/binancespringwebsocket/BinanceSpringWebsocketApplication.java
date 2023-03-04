e882378ffd563799d177b02eecb9dab87a780585
Veysel Öztürk <veyselozturk.6198@gmail.com>
2023-03-04 17:38:47 +0300

    First commit

---
+++ src/main/java/com/veyselozturk/binancespringwebsocket/BinanceSpringWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.veyselozturk.binancespringwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceSpringWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinanceSpringWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/veyselozturk/binancespringwebsocket/BinanceSpringWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veyselozturk.binancespringwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceSpringWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
